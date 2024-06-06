83fcbee09ceb7cdd232ee8a623f96e22d169e83a
btieszen <briantieszen4@gmail.com>
2024-06-06 01:06:26 -0700

    add initial project description

---
+++ adventure_log.py
@@ -0,0 +1,7 @@
+adventure_log.py
+def greet_coding_temple():
+    print("ahoy! Coding temple navigstors! Ready for todays adventure")
+    
+    if _name_=="main":
+        greet_coding_temple
+        
